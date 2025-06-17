ca4c374abbdc66128a67822472cc9e39c82ae5f1
Dennif <dennif.pa@gmail.com>
2025-06-16 23:33:47 -0700

    Task converted to a component with no state

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -3,27 +3,38 @@ import PropTypes from 'prop-types';
 
 import './Task.css';
 
-const Task = ({ id, title, isComplete }) => {
-  const [complete, setComplete] = useState(isComplete);
-  const buttonClass = complete ? 'tasks__item__toggle--completed' : 'tasks__item__toggle--incompleted';
+const Task = ({ id, title, isComplete, onToggle, onDelete }) => {
+  const buttonClass = isComplete ? 'tasks__item__toggle--completed'
+    : 'tasks__item__toggle--incompleted';
 
   return (
     <li className="tasks__item">
+      {/* WHEN click, CALL FUNTIONS BECAUSE THE PROPS */}
       <button
         className={`tasks__item__toggle ${buttonClass}`}
-        onClick={() => setComplete(!complete)}
+        onClick={() => onToggle(id)}
       >
         {title}
       </button>
-      <button className="tasks__item__remove button">x</button>
+
+      {/*  DELETE TASK */}
+      <button
+        className="tasks__item__remove button"
+        onClick={() => onDelete(id)}
+      >
+        x
+      </button>
     </li>
   );
 };
+
 
 Task.propTypes = {
   id: PropTypes.number.isRequired,
   title: PropTypes.string.isRequired,
   isComplete: PropTypes.bool.isRequired,
+  onToggle: PropTypes.func.isRequired,
+  onDelete: PropTypes.func.isRequired,
 };
 
 export default Task;
